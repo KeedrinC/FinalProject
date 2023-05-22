1384b29c604e4a4acdd5a5da5dbfd811647940cb
Keedrin Collins <keedrin@gmail.com>
2023-05-22 09:07:57 -0400

    Update TaxiDriverUnit.java
    - added valid actions
    - removed base class attributes
    - syntax

--- src/main/java/TaxiDriverUnit.java
+++ src/main/java/TaxiDriverUnit.java
@@ -1,48 +1,51 @@
-public class TaxiDriverUnit extends BartSimpsonUnit{
-    final int MAX_NUM_SPAWNED = 2;
-    private int numTimesSpawned;
-    private boolean recruit;
+// New Unit Modifications
+/**
+ * This class is for our Taxi Driver game piece. The Taxi Driver moves faster than other
+ * pieces, including Bart, and has the ability to recruit enemies when near.
+ */
 
+public class TaxiDriverUnit extends BartSimpsonUnit {
+    private final int MAX_NUM_SPAWNED = 2;
     public TaxiDriverUnit(char symbol, String name, double health, double healthModifier, double damage,
-                          double damageModifier, int luck, int xCor, int yCor, int movement,
-                          int movementModifier, int numTimesSpawned, boolean distract, boolean recruit) {
+        double damageModifier, int luck, int xCor, int yCor, int movement,
+        int movementModifier, int numTimesSpawned, boolean distract, boolean recruit) {
         super(symbol, name, health, healthModifier, damage, damageModifier, luck,
                 xCor, yCor, movement, movementModifier, numTimesSpawned, distract, recruit);
-
     }
+
     public TaxiDriverUnit() {
         this('D', "Taxi Driver", 100.0, 0.0, 25.0, 0.0, 0,
-                5, 5, 1, 0, 0,true, true);
+                5, 5, 3, 0, 0,true, true);
     }
-    public int getNumTimesSpawned() {
-        return this.numTimesSpawned;
-    }
-    public void setNumTimesSpawned(int numTimesSpawned) {
-        this.numTimesSpawned = numTimesSpawned;
-    }
-    public boolean canRecruit() {
-        return recruit;
-    }
-    public void setRecruit(boolean recruit) {
-        this.recruit = recruit;
-    }
+
     public boolean canSpawn() {
         return Character.isUpperCase(this.getSymbol())
-                && this.numTimesSpawned < this.MAX_NUM_SPAWNED;
+                && super.getNumTimesSpawned() < this.MAX_NUM_SPAWNED;
     }
+
     public TaxiDriverUnit spawn() {
         TaxiDriverUnit newDriver = new TaxiDriverUnit(
-                Character.toLowerCase(this.symbol), "Taxi Driver", 100.0, 5.0, 25.0, 10.0, 0, 1,
-                1, 1, 1, 0, true, true);
-        this.numTimesSpawned++;
+            Character.toLowerCase(this.symbol), "Taxi Driver", 100.0, 5.0, 25.0, 10.0, 0, 1,
+            1, 1, 1, 0, true, true);
+        super.setNumTimesSpawned(super.getNumTimesSpawned() + 1);
         return newDriver;
     }
-    public boolean validRecruitPath(int row, int column, int targetRow, int targetColumn) {
-        // can only recruit units that are 1 space away in any direction from a Taxi Driver
-        if (targetRow == row + 1 || targetColumn == column + 1
-            || targetRow == row - 1 || targetColumn == column - 1) {
-            return true;
-        }
-        return false;
+
+    @Override
+    public boolean validMovePath(int row, int column, int targetRow, int targetColumn) {
+        // can move 3 spaces away in any direction
+        return Math.abs(targetRow - row) <= 3 && Math.abs(targetColumn - column) <= 3;
     }
+
+    @Override
+    public boolean validRecruitPath(int row, int column, int targetRow, int targetColumn) {
+        // can recruit units that are 2 spaces away in any direction from a Taxi Driver
+        return Math.abs(targetRow - row) <= 2 && Math.abs(targetColumn - column) <= 2;
+    }
+    @Override
+	public boolean validSpawnPath(int row, int column, int targetRow, int targetColumn) {
+        // can spawn units that are 1 space away in the same row
+        if (row != targetRow) return false;
+        return Math.abs(targetRow - row) == 1;
+	}
 }
